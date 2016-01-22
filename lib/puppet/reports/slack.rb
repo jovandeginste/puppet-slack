9428792a6bf2ccf74a017e66d5911ac5bed10ee7
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2016-01-22 17:11:10 +0100

    Fix indentation

--- lib/puppet/reports/slack.rb
+++ lib/puppet/reports/slack.rb
@@ -5,45 +5,45 @@ require 'json'
 require 'uri'
 
 Puppet::Reports.register_report(:slack) do
-  desc 'Send notification of puppet run reports to Slack Messaging.'
+	desc 'Send notification of puppet run reports to Slack Messaging.'
 
-  def compose(config, message)
-      payload = {
-        'channel'  => config[:slack_channel],
-        'username' => config[:slack_botname],
-        'icon_url' => config[:slack_iconurl],
-        'text'     => message
-      }
-      JSON.generate(payload)
-  end
+	def compose(config, message)
+		payload = {
+			'channel'  => config[:slack_channel],
+			'username' => config[:slack_botname],
+			'icon_url' => config[:slack_iconurl],
+			'text'     => message
+		}
+		JSON.generate(payload)
+	end
 
-  def process
+	def process
 
-    # setup
-    configfile = File.join(Puppet.settings[:confdir], 'slack.yaml')
-    unless File.readable?(configfile)
-      msg = "Slack report config file #{configfile} is not readable."
-      raise(Puppet::ParseError, msg)
-    end
-    config = YAML.load_file(configfile)
-    slack_uri = URI.parse(config[:slack_url])
+		# setup
+		configfile = File.join(Puppet.settings[:confdir], 'slack.yaml')
+		unless File.readable?(configfile)
+			msg = "Slack report config file #{configfile} is not readable."
+			raise(Puppet::ParseError, msg)
+		end
+		config = YAML.load_file(configfile)
+		slack_uri = URI.parse(config[:slack_url])
 
-    # filter
-    #return if self.status == 'unchanged'
-    #return if self.status == 'changed'
-    status_icon = case self.status
-                        when 'changed' then ':sparkles:'
-                        when 'failed' then ':no_entry:'
-                        when 'unchanged' then ':white_check_mark:'
-                  end
-    # Refer: https://slack.zendesk.com/hc/en-us/articles/202931348-Using-emoji-and-emoticons
+		# filter
+		#return if self.status == 'unchanged'
+		#return if self.status == 'changed'
+		status_icon = case self.status
+									when 'changed' then ':sparkles:'
+									when 'failed' then ':no_entry:'
+									when 'unchanged' then ':white_check_mark:'
+									end
+		# Refer: https://slack.zendesk.com/hc/en-us/articles/202931348-Using-emoji-and-emoticons
 
-    # construct message
-    if config[:slack_puppetboard_url]
-      message = "#{status_icon} Puppet run for <#{config[:slack_puppetboard_url]}/node/#{self.host}|#{self.host}> #{self.status} at #{Time.now.asctime}."
-    else
-      message = "#{status_icon} Puppet run for #{self.host} #{self.status} at #{Time.now.asctime}."
-    end
+		# construct message
+		if config[:slack_puppetboard_url]
+			message = "#{status_icon} Puppet run for <#{config[:slack_puppetboard_url]}/node/#{self.host}|#{self.host}> #{self.status} at #{Time.now.asctime}."
+		else
+			message = "#{status_icon} Puppet run for #{self.host} #{self.status} at #{Time.now.asctime}."
+		end
 
 		important_facts = %w[ environment tier role subrole ]
 		fact_table = [
@@ -57,17 +57,17 @@ Puppet::Reports.register_report(:slack) do
 			fact_table
 		].flatten.join("\n")
 
-    Puppet.info "Sending status for #{self.host} to Slack."
+		Puppet.info "Sending status for #{self.host} to Slack."
 
-    conn = Faraday.new(:url => slack_uri.scheme + '://' + slack_uri.host) do |faraday|
-      faraday.request :url_encoded
-      faraday.adapter Faraday.default_adapter
-    end
+		conn = Faraday.new(:url => slack_uri.scheme + '://' + slack_uri.host) do |faraday|
+			faraday.request :url_encoded
+			faraday.adapter Faraday.default_adapter
+		end
 
-    conn.post do |req|
-      req.url slack_uri.path
-      req.body = "payload=" + compose(config, message)
-    end
+		conn.post do |req|
+			req.url slack_uri.path
+			req.body = "payload=" + compose(config, message)
+		end
 
-  end
+	end
 end
